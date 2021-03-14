c9b22b01c0a976a8c825b166e562651f8e142ce3
PeanutbutterWarrior <64+PeanutbutterWarrior@users.noreply.github.com>
2021-03-14 13:43:52 +0000

    Day 3 Part 2

--- 2020/Day3.py
+++ 2020/Day3.py
@@ -1,12 +1,26 @@
 with open('Day3.txt', 'r') as file:
     data = file.read().split()
 
+
+def check_slope(across, down):
+    x_position = 0
+    tree_count = 0
+    for index, line in enumerate(data):
+        if index % down != 0:
+            continue
+        if line[x_position] == '#':
+            tree_count += 1
+        x_position = (x_position + across) % len(line)
+    return tree_count
+
+
 # Part 1
 
-x_position = 0
-tree_count = 0
-for line in data:
-    if line[x_position] == '#':
-        tree_count += 1
-    x_position = (x_position + 3) % len(line)
-print(tree_count)
+print(check_slope(3, 1))
+
+# Part 2
+combinations = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+product = 1
+for combo in combinations:
+    product *= check_slope(*combo)
+print(product)
