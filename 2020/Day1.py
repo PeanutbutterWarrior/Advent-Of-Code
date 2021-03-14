7b991210f09c74982e9ed2b5b51c326b7eba1720
PeanutbutterWarrior <64+PeanutbutterWarrior@users.noreply.github.com>
2021-03-14 15:46:30 +0000

    Fix answer for part 2 printing multiple times

--- 2020/Day1.py
+++ 2020/Day1.py
@@ -9,7 +9,7 @@ for ind1, val1 in enumerate(data):
 
 # Part 2
 for ind1, val1 in enumerate(data):
-    for ind2, val2 in enumerate(data[ind1 + 1:]):
-        for ind3, val3 in enumerate(data[ind2 + 1:]):
+    for ind2, val2 in enumerate(data[ind1 + 1:], ind1 + 1):
+        for ind3, val3 in enumerate(data[ind2 + 1:], ind2 + 1):
             if val1 + val2 + val3 == 2020:
                 print(f'{val1} * {val2} * {val3} = {val1 * val2 * val3}')
