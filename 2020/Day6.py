5f2178476f44015fd137e0681e2c6b13392b4ac1
PeanutbutterWarrior <64+PeanutbutterWarrior@users.noreply.github.com>
2021-03-14 14:37:57 +0000

    Day 6 Part 2

--- 2020/Day6.py
+++ 2020/Day6.py
@@ -2,6 +2,7 @@ with open('Day6.txt', 'r') as file:
     data = file.read().split('\n')
 
 # Part 1
+
 count = 0
 answers = [False] * 26
 for line in data:
@@ -11,4 +12,24 @@ for line in data:
     else:
         count += answers.count(True)
         answers = [False] * 26
+print(count)
+
+# Part 2
+
+
+def convert_to_num(answer):
+    num = 0
+    for char in answer:
+        num |= 1 << ord(char) - 97
+    return num
+
+
+count = 0
+answers = 2 ** 26 - 1
+for line in data:
+    if line:
+        answers &= convert_to_num(line)
+    else:
+        count += bin(answers).count('1')
+        answers = 2 ** 26 - 1
 print(count)
