70979b59eeded6b2dc9caa5d79d4fae93cb0f79c
PeanutbutterWarrior <64+PeanutbutterWarrior@users.noreply.github.com>
2021-03-14 13:32:55 +0000

    Day 2 Part 2

--- 2020/Day1.py
+++ 2020/Day1.py
@@ -1,11 +1,13 @@
 with open('Day1.txt', 'r') as file:
     data = list(map(int, file.read().split()))
 
+# Part 1
 for ind1, val1 in enumerate(data):
     for ind2, val2 in enumerate(data[ind1 + 1:]):
         if val1 + val2 == 2020:
             print(f'{val1} * {val2} = {val1 * val2}')
 
+# Part 2
 for ind1, val1 in enumerate(data):
     for ind2, val2 in enumerate(data[ind1 + 1:]):
         for ind3, val3 in enumerate(data[ind2 + 1:]):
--- 2020/Day2.py
+++ 2020/Day2.py
@@ -1,6 +1,7 @@
 with open('Day2.txt', 'r') as file:
     data = file.read().split('\n')
 
+# Part 1
 num_correct_password = 0
 for password in data:
     if password:
@@ -11,5 +12,19 @@ for password in data:
         letter = password[space_ind + 1]
         word = password[space_ind + 4:]
         if minimum <= word.count(letter) <= maximum:
+            num_correct_password += 1
+print(num_correct_password)
+
+# Part 2
+num_correct_password = 0
+for password in data:
+    if password:
+        dash_ind = password.index('-')
+        space_ind = password.index(' ')
+        ind1 = int(password[:dash_ind])
+        ind2 = int(password[dash_ind + 1:space_ind])
+        letter = password[space_ind + 1]
+        word = password[space_ind + 4:]
+        if (word[ind1 - 1] == letter) ^ (word[ind2 - 1] == letter):
             num_correct_password += 1
 print(num_correct_password)
