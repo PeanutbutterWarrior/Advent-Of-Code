aba49212272ff979f1394ca3fd1b119249453658
PeanutbutterWarrior <50717143+PeanutbutterWarrior@users.noreply.github.com>
2021-03-14 17:51:19 +0000

    Day 12 Part 2

--- 2020/Day12.py
+++ 2020/Day12.py
@@ -1,6 +1,9 @@
 with open('Day12.txt', 'r') as file:
     data = file.read().split()
 
+
+# Part 1
+
 x = 0
 y = 0
 direction = 0
@@ -32,3 +35,33 @@ for command in data:
         direction = (direction + arg // 90) % 4
 
 print(abs(x) + abs(y))
+
+# Part 2
+
+x = 10
+y = 1
+ax = 0
+ay = 0
+
+for command in data:
+    action = command[0]
+    arg = int(command[1:])
+    if action == 'N':
+        y += arg
+    elif action == 'S':
+        y -= arg
+    elif action == 'W':
+        x -= arg
+    elif action == 'E':
+        x += arg
+    elif action == 'L':
+        for i in range(arg//90):
+            x, y = -y, x
+    elif action == 'R':
+        for i in range(arg//90):
+            x, y = y, -x
+    elif action == 'F':
+        ax += x * arg
+        ay += y * arg
+
+print(abs(ax) + abs(ay))
