752b76dac8449983fa07d96a79eaee323d9571dc
PeanutbutterWarrior <64+PeanutbutterWarrior@users.noreply.github.com>
2021-03-14 15:11:05 +0000

    Day 7 Part 1

---
+++ 2020/Day7.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+
+with open('Day7.txt', 'r') as file:
+    data = file.read().split('\n')
+
+
+def parse_line(line):
+    line = line.split(' ')
+    parent = f'{line[0]} {line[1]}'
+    if line[4] == 'no':
+        return parent, []
+    children_words = line[4:]
+    children = []
+    for i in range(0, len(children_words), 4):
+        num, name1, name2, _ = children_words[i:i + 4]
+        children.append((f'{name1} {name2}', int(num)))
+    return parent, children
+
+
+# Part 1
+
+parents = defaultdict(list)
+for rule in data:
+    if rule:
+        parent, children = parse_line(rule)
+        for child in children:
+            parents[child[0]].append(parent)
+
+possible_parents = set()
+unchecked_parents = ['shiny gold']
+while len(unchecked_parents) > 0:
+    child = unchecked_parents.pop(-1)
+    for parent in parents[child]:
+        if parent not in possible_parents:
+            possible_parents.add(parent)
+            unchecked_parents.append(parent)
+print(len(possible_parents))
+
