5af1b2c9cd72b7218a7acbd15be6db4a13d3e80d
PeanutbutterWarrior <64+PeanutbutterWarrior@users.noreply.github.com>
2021-03-14 16:11:31 +0000

    Day 10 Part 2

--- 2020/Day10.py
+++ 2020/Day10.py
@@ -1,9 +1,12 @@
 with open('Day10.txt', 'r') as file:
     data = sorted(map(int, file.read().split()))
 
+data.insert(0, 0)  # Source
+data.append(max(data) + 3)  # Device
+
 # Part 1
 
-one_jolt_diffs = 1
+one_jolt_diffs = 0
 three_jolt_diffs = 0
 prev = -5
 for adaptor in data:
@@ -12,5 +15,29 @@ for adaptor in data:
     elif adaptor - prev == 3:
         three_jolt_diffs += 1
     prev = adaptor
-three_jolt_diffs += 1  # Going from final adaptor to device
 print(one_jolt_diffs * three_jolt_diffs)
+
+# Part 2
+
+data = set(data)
+checked = {}
+
+
+def paths_to(number):
+    if number in checked:
+        return checked[number]
+    if number == 0:
+        return 1
+
+    ways_here = 0
+    if number - 1 in data:
+        ways_here += paths_to(number - 1)
+    if number - 2 in data:
+        ways_here += paths_to(number - 2)
+    if number - 3 in data:
+        ways_here += paths_to(number - 3)
+    checked[number] = ways_here
+    return ways_here
+
+
+print(paths_to(max(data)))
