8647d95687c57a5da4208367b6dc56aa2f24f0ee
PeanutbutterWarrior <50717143+PeanutbutterWarrior@users.noreply.github.com>
2021-03-14 17:15:49 +0000

    Day 11 Part 2

--- 2020/Day11.py
+++ 2020/Day11.py
@@ -4,6 +4,8 @@ with open('Day11.txt', 'r') as file:
     data = list(map(list, file.read().split()))
 
 
+# Part 1
+
 def count_neighbours(x, y, data):
     count = 0
     for dx in range(-1, 2):
@@ -21,8 +23,6 @@ def count_neighbours(x, y, data):
                     pass
     return count
 
-
-# Part 1
 
 board = deepcopy(data)
 WIDTH = len(board[0])
@@ -51,6 +51,66 @@ while len(changed) > 0:
                 for dy in range(-1, 2):
                     for dx in range(-1, 2):
                         new_changed.add((x + dx, y + dy))
+    changed = new_changed
+    board = new_board
+
+count = 0
+for line in board:
+    count += line.count('#')
+print(count)
+
+
+# Part 2
+
+def count_neighbours(x, y, data):
+    count = 0
+    neighbours = set()
+    for dx in range(-1, 2):
+        for dy in range(-1, 2):
+            if not dx == dy == 0:
+                ax = x + dx
+                ay = y + dy
+                try:
+                    while data[ay][ax] == '.':
+                        ax += dx
+                        ay += dy
+                        if ax < 0 or ay < 0:
+                            break
+                    else:
+                        if ax < 0 or ay < 0:
+                            continue
+                        neighbours.add((ax, ay))
+                        count += int(data[ay][ax] == '#')
+                except IndexError:
+                    continue
+    return count, neighbours
+
+
+board = deepcopy(data)
+WIDTH = len(board[0])
+HEIGHT = len(board)
+changed = set((x, y) for y in range(HEIGHT) for x in range(WIDTH))
+
+while len(changed) > 0:
+    new_board = deepcopy(board)
+    new_changed = set()
+    for x, y in changed:
+        if x < 0 or y < 0:
+            continue
+        try:
+            tile = board[y][x]
+        except IndexError:
+            continue
+        if tile != '.':
+            num_neighbours, neighbours = count_neighbours(x, y, board)
+            if tile == 'L' and num_neighbours == 0:
+                new_board[y][x] = '#'
+                new_changed |= neighbours
+                new_changed.add((x, y))
+            elif tile == '#' and num_neighbours >= 5:
+                new_board[y][x] = 'L'
+                new_changed |= neighbours
+                new_changed.add((x, y))
     changed = new_changed
     board = new_board
 
