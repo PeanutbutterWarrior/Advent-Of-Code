6bf30bbd20db126112ca26ad92d02e2f24e0ab89
PeanutbutterWarrior <50717143+PeanutbutterWarrior@users.noreply.github.com>
2021-03-16 22:39:11 +0000

    Day 13 Part 2

--- 2020/Day13.py
+++ 2020/Day13.py
@@ -1,16 +1,42 @@
 with open('Day13.txt') as file:
     data = file.read().split()
     time = int(data[0])
-    buses = list(map(int, filter(str.isdigit, data[1].split(','))))
+    buses = data[1].split(',')
 
 # Part 1
 
 min_wait = float('inf')
 min_bus = -1
 for schedule in buses:
+    if schedule == 'x':
+        continue
+    schedule = int(schedule)
     wait = (time // schedule + 1) * schedule - time
     if wait < min_wait:
         min_wait = wait
         min_bus = schedule
 
 print(min_bus * min_wait)
+
+# Part 2
+
+times = []
+for ind, val in enumerate(buses):
+    if val != 'x':
+        val = int(val)
+        times.append((val, val - (ind % val)))
+
+step, count = times.pop(0)
+times = sorted(times, key=lambda a: a[0])
+looking, modulo = times.pop(0)
+
+while True:
+    if count % looking == modulo:
+        step *= looking
+        if len(times) == 0:
+            break
+        looking, modulo = times.pop(0)
+    count += step
+
+print(count)
+
