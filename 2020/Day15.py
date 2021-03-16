5bf824c93a7f25e8152ea04750ef8f6257cf4cf1
PeanutbutterWarrior <50717143+PeanutbutterWarrior@users.noreply.github.com>
2021-03-16 23:29:47 +0000

    Day 15 Part 2

--- 2020/Day15.py
+++ 2020/Day15.py
@@ -4,16 +4,30 @@ with open('Day15.txt', 'r') as file:
 # Part 1
 
 last_time_said = {val: (None, ind) for ind, val in enumerate(times)}
-previous_number = times[-1]
+number = times[-1]
 
 for turn in range(len(times), 2020):
-    first, second = last_time_said.get(previous_number, (None, None))
+    first, second = last_time_said.get(number, (None, None))
     if first is None:
         number = 0
     else:
         number = second - first
     last_time_said[number] = (last_time_said.get(number, (None, None))[1], turn)
-    print(number)
-    previous_number = number
 
-print(previous_number)
+print(number)
+
+# Part 2
+
+indexes = {val: ind for ind, val in enumerate(times)}
+
+num = 0
+for turn in range(len(times), 30000000 - 1):
+    last_index = indexes.get(num, None)
+    indexes[num] = turn
+    if last_index is not None:
+        num = turn - last_index
+    else:
+        num = 0
+
+print(num)
+
