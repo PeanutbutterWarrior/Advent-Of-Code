c72047642a79bf66016c72513a588e83f4ca78e0
PeanutbutterWarrior <50717143+PeanutbutterWarrior@users.noreply.github.com>
2021-03-17 00:14:10 +0000

    Day 16 Part 2

--- 2020/Day16.py
+++ 2020/Day16.py
@@ -38,3 +38,44 @@ for ticket in tickets:
 
 print(error_total)
 
+# Part 2
+
+valid_tickets = []
+
+for ticket in tickets:
+    for field in ticket:
+        for rule in rules.values():
+            if field in rule[0] or field in rule[1]:
+                break
+        else:
+            break
+    else:
+        valid_tickets.append(ticket)
+
+tickets = valid_tickets
+possible_fields = [set(rules.keys()) for i in range(len(own_ticket_fields))]
+
+for ticket in tickets:
+    for rule, (range1, range2) in rules.items():
+        for ind, field in enumerate(ticket):
+            if field not in range1 and field not in range2:
+                possible_fields[ind].discard(rule)
+
+changed = True
+rule_order = [None] * len(possible_fields)
+while changed:
+    changed = False
+    for ind, possible in enumerate(possible_fields):
+        if len(possible) == 1:
+            changed = True
+            item = possible.pop()
+            rule_order[ind] = item
+            for i in possible_fields:
+                if i is not possible:
+                    i.discard(item)
+
+product = 1
+for rule, field in zip(rule_order, own_ticket_fields):
+    if rule[:9] == 'departure':
+        product *= field
+print(product)
