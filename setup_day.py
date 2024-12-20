256de1b454363762300473142b670b1e6997a285
PeanutbutterWarrior <50717143+PeanutbutterWarrior@users.noreply.github.com>
2024-12-20 17:02:29 +0000

    Also grab test data

--- setup_day.py
+++ setup_day.py
@@ -4,6 +4,7 @@ import argparse
 import datetime
 from enum import Enum, auto
 from pathlib import Path
+from bs4 import BeautifulSoup
 
 class Language(Enum):
     PYTHON = auto(), "py"
@@ -39,8 +40,7 @@ def load_headers(args):
     with open(args.headers, "r") as file:
         return json.load(file)
 
-def get_input_file(args):
-    url = f"https://adventofcode.com/{args.year}/day/{args.day}/input"
+def make_request(url):
     headers = load_headers(args)
     response = requests.get(url, headers=headers)
     if response.status_code != 200:
@@ -49,6 +49,17 @@ def get_input_file(args):
         quit()
     return response.content.decode().strip()
 
+def get_input_file(args):
+    url = f"https://adventofcode.com/{args.year}/day/{args.day}/input"
+    return make_request(url)
+
+def get_test_file(args):
+    url = f"https://adventofcode.com/{args.year}/day/{args.day}"
+    data = make_request(url)
+    soup = BeautifulSoup(data, "html.parser")
+    code_block = soup.find("pre").find("code")
+    return code_block.get_text()
+
 def get_current_folder(args):
     path = Path(str(args.year), f"Day{args.day}")
     path.mkdir(parents=True, exist_ok=True)
@@ -56,13 +67,17 @@ def get_current_folder(args):
 
 def get_template(args):
     with open(Path("templates", f"{args.lang.ext}.txt")) as file:
-        return file.read()
+        data = file.read()
+    
+    data = data.replace("{{day}}", str(args.day)).replace("{{year}}", str(args.year))
+    return data
 
 def write_files(args):
     folder = get_current_folder(args)
     day_name = f"Day{args.day}"
     (folder / f"{day_name}.{args.lang.ext}").write_text(get_template(args))
-    (folder / f"{day_name}.txt").write_text(get_input_file(args))
+    (folder / "input.txt").write_text(get_input_file(args))
+    (folder / "test.txt").write_text(get_test_file(args))
 
 if __name__ == "__main__":
     args = get_args()
