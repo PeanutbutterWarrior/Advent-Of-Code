d1099cb9d3ff4d9cebfed611c2bc8f4e66fea609
PeanutbutterWarrior <50717143+PeanutbutterWarrior@users.noreply.github.com>
2024-12-28 20:24:11 +0000

    Improve handling of crashing programs

--- submit.py
+++ submit.py
@@ -1,5 +1,4 @@
 import argparse
-from enum import Enum, auto
 import datetime
 import subprocess
 import os
@@ -39,13 +38,13 @@ def get_current_folder(args):
 def run_python(args):
     command = f"python Day{args.day}.py"
     if args.file is not None:
-        command += " " + args.file
+        file = args.file
     elif args.test:
-        command += " test.txt"
+        file = "test.txt"
     else:
-        command += " input.txt"
+        file = "input.txt"
 
-    return subprocess.run(command, capture_output=True)
+    return subprocess.run(command, file, capture_output=True)
 
 def run_c(args):
     proc = subprocess.run(f"gcc -o out.exe Day{args.day}.c", capture_output=True)
@@ -62,12 +61,16 @@ def run_rust(args):
 
 def run_program(args):
     os.chdir(get_current_folder(args))
+    run_func = None
     if args.lang == Language.PYTHON:
-        proc = run_python(args)
+        run_func = run_python
     elif args.lang == Language.C:
-        proc = run_c(args)
+        run_func = run_c
     elif args.lang == Language.RUST:
-        proc = run_rust(args)
+        run_func = run_rust
+    
+    proc = run_func(args)
+    print("Execution interrupted")
 
     if proc.returncode != 0:
         print(proc.stdout.decode())
@@ -75,12 +78,7 @@ def run_program(args):
         exit(1)
     return proc.stdout.decode().strip().split("\r\n")
 
-def submit_value(args, value):
-    ...
-
-if __name__ == "__main__":
-    args = get_args()
-    ans = run_program(args)
+def print_output(args, ans):
     if len(ans) == 1:
         ans1 = ans[0]
         ans2 = None
@@ -95,5 +93,12 @@ if __name__ == "__main__":
     if ans2 is not None:
         print(f"{args.year} Day {args.day} Part 2: {ans2}")
 
+def submit_value(args, value):
+    ...
+
+if __name__ == "__main__":
+    args = get_args()
+    ans = run_program(args)
+    print_output(args, ans)
     if args.submit:
-        submit_value(args, ans1)
+        submit_value(args, ans)
