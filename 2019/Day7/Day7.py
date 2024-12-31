17de243e04fbcf09a8f6b1c95a484b889a28ad28
PeanutbutterWarrior <50717143+PeanutbutterWarrior@users.noreply.github.com>
2024-12-31 18:00:34 +0000

    Complete Day 7 part 2

--- 2019/Day7/Day7.py
+++ 2019/Day7/Day7.py
@@ -15,3 +15,19 @@ for perm in permutations([0, 1, 2, 3, 4], r=5):
         signal = interp.run()[0]
     max_signal = max(max_signal, signal)
 print(max_signal)
+
+max_signal = 0
+for perm in permutations([5, 6, 7, 8, 9], r=5):
+    comps = [Intcode(base_mem.copy(), [perm[i]]) for i in range(5)]
+    for comp in comps:
+        comp.run()
+    
+    signal = 0
+    last_signal = 0
+    while comps[-1].running:
+        for comp in comps:
+            comp.give_input(signal)
+            signal = comp.run()[-1]
+    max_signal = max(signal, max_signal)
+print(max_signal)
+    
